17ec98fa476c88e89a2e371725dd661d66b24c77
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-05-31 23:16:11 -0500

    funciona contador de caracteres

--- js/app.js
+++ js/app.js
@@ -1,41 +1,16 @@
-var target = 0;
+var contadorCaracteres = 0;
 
 var cargarPagina = function () {
-	var botones = $(".control");
-	var anterior = $(".anterior");
-	var siguiente = $(".siguiente");
-	
-	botones.click(cambiarImagen);
-	anterior.click(anteriorImagen);
-	siguiente.click(siguienteImagen);
-};
+    var areaTexto = $("#areaTexto");
+    var areaAutor = $("#areaAutor");
+    var botonEnviar = $("#botonEnviar");
+    areaTexto.keyup(contadoraDeCaracteres);
 
-var cambiarImagen = function () {
-	target = parseInt($(this).data("target"));
-    //console.log(target)
-	mostrarImagen(target);
+    function contadoraDeCaracteres() {
+        contadorCaracteres = 140 - (areaTexto.val().length);
+        var cantidadCaracteres = $("#cantidadCaracteres");
+        cantidadCaracteres.text(contadorCaracteres +"/140");
+    };
 };
-
-var mostrarImagen = function (target) {
-	var imagenActual = $("div.activo");
-	var imagenSiguiente = $("div[data-imagen='" + target + "']");
-	imagenActual.removeClass("activo");
-	imagenSiguiente.addClass("activo");
-};
-
-var anteriorImagen = function (e) {
-	e.preventDefault();
-	target = target - 1;
-	target = (target < 0) ? 3 : target;
-	mostrarImagen(target);
-};
-
-var siguienteImagen = function (e) {
-	e.preventDefault();
-	target = target + 1;
-	target = (target > 3) ? 0 : target;
-	mostrarImagen(target);mostrarImagen(target);
-};
-
 
 $(document).ready(cargarPagina);
