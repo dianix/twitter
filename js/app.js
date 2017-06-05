936d4aedcbb81f47ac6e1834ad719973d0753636
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-06-04 20:33:27 -0500

    agregada función para indicar hora de publicación

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     var contadorCaracteres = 0;
 })();
 
@@ -8,7 +8,6 @@ var cargarPagina = function () {
     var botonEnviar = $("#botonEnviar");
     areaTexto.keyup(contadoraDeCaracteres);
     areaTexto.keyup(validarMensaje);
-    
     botonEnviar.click(publicarTwit);
 };
 
@@ -17,17 +16,16 @@ var contadoraDeCaracteres = function () {
     //contador que indica en forma regresiva los caracteres tecleados
     contadorCaracteres = 140 - ($(areaTexto).val().length);
     // condicionales para el color de texto del contador
-    if (contadorCaracteres >= 21 && contadorCaracteres <= 30){
-        cantidadCaracteres.text(contadorCaracteres + "/140").css("color","orange");        
+    if (contadorCaracteres >= 21 && contadorCaracteres <= 30) {
+        cantidadCaracteres.text(contadorCaracteres + "/140").css("color", "orange");
     }
-    if(contadorCaracteres >= 0 && contadorCaracteres <= 20){
-        cantidadCaracteres.text(contadorCaracteres + "/140").css("color","red");
+    if (contadorCaracteres >= 0 && contadorCaracteres <= 20) {
+        cantidadCaracteres.text(contadorCaracteres + "/140").css("color", "red");
     }
-    if (contadorCaracteres < 0){
-        cantidadCaracteres.text(contadorCaracteres + "/140").css("color","silver");
-    }
-    else if (contadorCaracteres >= 31 && contadorCaracteres <= 140){
-        cantidadCaracteres.text(contadorCaracteres + "/140").css("color","black");
+    if (contadorCaracteres < 0) {
+        cantidadCaracteres.text(contadorCaracteres + "/140").css("color", "silver");
+    } else if (contadorCaracteres >= 31 && contadorCaracteres <= 140) {
+        cantidadCaracteres.text(contadorCaracteres + "/140").css("color", "black");
     }
 };
 
@@ -38,19 +36,19 @@ var publicarTwit = function () {
     var tarjetaPublicacion = $("<article/>").addClass("card");
     var twitNuevo = $("<p/>").text($(areaTexto).val());
     var autorTwit = $(document.createElement("h5")).text($(areaAutor).val());
+    var horaTwit = $("<h6/>").text(fechaPublicación());
     // Publicando 
-    tarjetaPublicacion.append($(autorTwit)).append($(twitNuevo));
+    tarjetaPublicacion.append($(autorTwit)).append($(twitNuevo)).append($(horaTwit));
     seccionPublicaciones.prepend($(tarjetaPublicacion));
     // Reinicializar valores
     limpiarAreas();
-    $(cantidadCaracteres).text("140/140").css("color","black");
-
+    $(cantidadCaracteres).text("140/140").css("color", "black");
 }
 
-var limpiarAreas = function() {
+var limpiarAreas = function () {
     $(areaTexto).val("");
     $(areaAutor).val("");
-}
+};
 
 var validarMensaje = function () {
     var $botonEnviar = $("#botonEnviar");
@@ -60,5 +58,18 @@ var validarMensaje = function () {
         $botonEnviar.attr("disabled", true);
     }
 };
+
+var fechaPublicación = function () {
+    var tiempoActual = new Date();
+    var fecha = tiempoActual.getDate() + "/" +
+        (tiempoActual.getMonth() + 1) + "/" +
+        tiempoActual.getFullYear();
+    var hora = tiempoActual.getHours();
+    var minutos = tiempoActual.getMinutes();
+    minutos = minutos <= 9 ? "0"+minutos : minutos;
+    var fechaHora = fecha + " " + hora + ":" + minutos;
+    return (fechaHora);
+}
+
 
 $(document).ready(cargarPagina);
